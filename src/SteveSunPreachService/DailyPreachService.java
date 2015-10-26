e8263cf37092761b042fc2ef5dc8e906eeb04d9e
Steve Sun <jiahuan@amazon.com>
2015-10-26 07:23:09 -0700

    added SteveSunCoolPreachService project

---
+++ src/SteveSunPreachService/DailyPreachService.java
@@ -0,0 +1,9 @@
+package SteveSunPreachService;
+
+import java.io.Serializable;
+
+public interface DailyPreachService  extends Serializable{
+    public void dailyStudyBible();
+    
+    public void doDailyFellowship();
+}
